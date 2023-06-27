392a492dbe7a1ea2a49bcb1cb7bc2ced250980ea
semensid2020 <semensid2020@gmail.com>
2023-06-27 22:44:07 +0700

    Admin workspace 3: users INDEX 1. Добавлен view для вывода всех пользователей и поле для поиска по email 2. Добавлен метод для экшна INDEX и тест на открытие страницы /admin/users. Задействован Ransack для поиска 3. Дополнительно потребовалось определить метод класса ransackable_attributes для рендера view'шки index.html.slim 4. Немного rubocop

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,9 @@
 class Admin::UsersController < Admin::ApplicationController
+  def index
+    @q = User.ransack(params[:q])
+    @users = @q.result.order(:type).order(:id).page(params[:page]).per(params[:per_page])
+  end
+
   def show
     @user = User.find(params[:id])
   end
