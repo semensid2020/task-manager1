fbbf094cde6e8facdf5807a61a8ef8f448baf297
semensid2020 <semensid2020@gmail.com>
2023-06-29 22:54:14 +0700

    API v1: INDEX SHOW CREATE methods

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,2 +1,30 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
+  def index
+    tasks = Task.all
+                .ransack(ransack_params)
+                .result
+                .page(page)
+                .per(per_page)
+
+    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
+
+  def show
+    task = Task.find(params[:id])
+
+    respond_with(task, serializer: TaskSerializer)
+  end
+
+  def create
+    task = current_user.my_tasks.new(task_params)
+    task.save
+
+    respond_with(task, serializer: TaskSerializer, location: nil)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description, :author_id, :assignee_id, :state_event)
+  end
 end
