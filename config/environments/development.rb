18b970a62080153b05eef9e1680be3d5317132f3
semensid2020 <semensid2020@gmail.com>
2023-07-24 23:33:40 +0700

    Uffizzi previews settings added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # Previewing PRs code running in sandbox at https://uffizzi.com/
+  config.hosts << '.app.uffizzi.com'
 end
